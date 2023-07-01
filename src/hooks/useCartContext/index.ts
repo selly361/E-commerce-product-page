d3045eb615b71575914929be4e32036bf0671634
seikou <seikou615@gmail.com>
2023-07-01 12:36:24 +0100

    Added: `useCartContext` hook

---
+++ src/hooks/useCartContext/index.ts
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { CartContext } from "contexts/CartContext";
+
+const useCartContext = () => {
+  const context = useContext(CartContext);
+
+  if (context === undefined) {
+    throw new Error("useCartContext must be used within a CartContextProvider");
+  }
+
+  return context
+};
+
+export default useCartContext;
