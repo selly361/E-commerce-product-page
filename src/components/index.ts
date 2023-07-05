6a269205e8e2d02820ad39892e350e443f39f6bf
seikou <seikou615@gmail.com>
2023-07-05 20:18:07 +0100

    Exported `Main`, `Article`, and `ImagePreview` components as default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Header } from "./Header"
 export { default as Cart } from "./Cart"
+export { default as Main } from "./Main"
+export { default as Article } from "./Article"
+export { default as ImagePreview } from "./ImagePreview"
