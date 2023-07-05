5bd1508b6da46fc053107c1b0f2579572cdcc4e2
seikou <seikou615@gmail.com>
2023-07-05 20:15:02 +0100

    Added: `Main` component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,14 @@
+import { StyledMain } from './Main.styles'
+import { ImagePreview, Article } from "components"
+
+const Main = () => {
+
+  return (
+    <StyledMain>
+        <ImagePreview />
+        <Article />
+    </StyledMain>
+  )
+}
+
+export default Main
