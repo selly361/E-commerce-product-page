2f477ae7790f52d9c61f8f5848a6ef297af0bd71
seikou <seikou615@gmail.com>
2023-07-05 20:15:26 +0100

    Added: `Main` components' styles

---
+++ src/components/Main/Main.styles.ts
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const StyledMain = styled.main`
+    min-height: calc(100vh - 112px);
+    width: 100vw;
+    
+    display: grid;
+    grid-template-columns: 445px 445px;
+    column-gap: 125px; 
+    justify-content: center;
+    align-items: center;
+    padding-top: 90px;
+`
