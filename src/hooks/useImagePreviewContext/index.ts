d89743643fc683e95585ff39d9bfec2cac47c5dc
seikou <seikou615@gmail.com>
2023-07-09 12:45:54 +0100

    Fixed typo

--- src/hooks/useImagePreviewContext/index.ts
+++ src/hooks/useImagePreviewContext/index.ts
@@ -1,11 +1,11 @@
-import { useContext } from "react";
 import { ImagePreviewContext } from "contexts/ImagePreviewContext";
+import { useContext } from "react";
 
 export const useImagePreviewContext = () => {
   const context = useContext(ImagePreviewContext);
 
   if (context === undefined) {
-    throw new Error("useImagePreviewContext must be used within a ImagePreviewContexProvider");
+    throw new Error("useImagePreviewContext must be used within a ImagePreviewContextProvider");
   }
 
   return context
